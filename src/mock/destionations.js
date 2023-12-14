d5488136d962a6cb87b36a184b80d02211e42a95
Darina Smorkalova <darina.smorkalova@gmail.com>
2023-12-14 12:12:53 +0300

    Добавила моки

---
+++ src/mock/destionations.js
@@ -0,0 +1,126 @@
+const destinations = [
+  {
+    id: 'a68720f8-ce39-4ed3-898d-ea9f50c0afec',
+    description: '',
+    name: 'Munich',
+    pictures: []
+  },
+  {
+    id: 'd19c50c0-d853-4743-834e-e212a3d5cff7',
+    description: '',
+    name: 'Chamonix',
+    pictures: []
+  },
+  {
+    id: '8525951b-0798-4720-90cb-e255138f63aa',
+    description: 'Geneva - middle-eastern paradise',
+    name: 'Geneva',
+    pictures: [
+      {
+        src: 'https://22.objects.htmlacademy.pro/static/destinations/18.jpg',
+        description: 'Geneva with a beautiful old town'
+      }
+    ]
+  },
+  {
+    id: '3fe95771-be9c-4f32-a850-f4ecf2907f77',
+    description: 'Oslo - is a beautiful city',
+    name: 'Oslo',
+    pictures: [
+      {
+        src: 'https://22.objects.htmlacademy.pro/static/destinations/2.jpg',
+        description: 'Oslo full of of cozy canteens where you can try the best coffee in the Middle East'
+      }
+    ]
+  },
+  {
+    id: '4e890f47-a8d3-4efc-9ed4-2e2bf2eeadf5',
+    description: 'Frankfurt - a true asian pearl',
+    name: 'Frankfurt',
+    pictures: [
+      {
+        src: 'https://22.objects.htmlacademy.pro/static/destinations/1.jpg',
+        description: 'Frankfurt in a middle of Europe'
+      },
+      {
+        src: 'https://22.objects.htmlacademy.pro/static/destinations/14.jpg',
+        description: 'Frankfurt for those who value comfort and coziness'
+      }
+    ]
+  },
+  {
+    id: '14c3101b-69b0-4a44-9d13-eb881a8a6e85',
+    description: '',
+    name: 'Milan',
+    pictures: []
+  },
+  {
+    id: 'e6a77307-ac42-4d4c-92fb-684b32157647',
+    description: 'Madrid - is a beautiful city',
+    name: 'Madrid',
+    pictures: [
+      {
+        src: 'https://22.objects.htmlacademy.pro/static/destinations/14.jpg',
+        description: 'Madrid for those who value comfort and coziness'
+      },
+      {
+        src: 'https://22.objects.htmlacademy.pro/static/destinations/3.jpg',
+        description: 'Madrid with crowded streets'
+      },
+      {
+        src: 'https://22.objects.htmlacademy.pro/static/destinations/9.jpg',
+        description: 'Madrid with an embankment of a mighty river as a centre of attraction'
+      },
+      {
+        src: 'https://22.objects.htmlacademy.pro/static/destinations/5.jpg',
+        description: 'Madrid with a beautiful old town'
+      }
+    ]
+  },
+  {
+    id: 'd6410253-2c93-4f8d-a707-ccc143166bf4',
+    description: 'Nagasaki - a true asian pearl',
+    name: 'Nagasaki',
+    pictures: [
+      {
+        src: 'https://22.objects.htmlacademy.pro/static/destinations/19.jpg',
+        description: 'Nagasaki a perfect place to stay with a family'
+      },
+      {
+        src: 'https://22.objects.htmlacademy.pro/static/destinations/3.jpg',
+        description: 'Nagasaki in a middle of Europe'
+      },
+      {
+        src: 'https://22.objects.htmlacademy.pro/static/destinations/10.jpg',
+        description: 'Nagasaki middle-eastern paradise'
+      }
+    ]
+  },
+  {
+    id: '725641a2-d0e6-42f5-9575-09dc2d20cf76',
+    description: 'Saint Petersburg - is a beautiful city',
+    name: 'Saint Petersburg',
+    pictures: []
+  },
+  {
+    id: '7f5e1612-f9bd-48a0-ba84-070f8a690f63',
+    description: 'Naples - with a beautiful old town',
+    name: 'Naples',
+    pictures: [
+      {
+        src: 'https://22.objects.htmlacademy.pro/static/destinations/20.jpg',
+        description: 'Naples a true asian pearl'
+      },
+      {
+        src: 'https://22.objects.htmlacademy.pro/static/destinations/6.jpg',
+        description: 'Naples with a beautiful old town'
+      },
+      {
+        src: 'https://22.objects.htmlacademy.pro/static/destinations/9.jpg',
+        description: 'Naples with an embankment of a mighty river as a centre of attraction'
+      }
+    ]
+  }
+];
+
+export { destinations };
---
+++ src/util.js
@@ -0,0 +1,26 @@
+import dayjs from 'dayjs';
+
+const DATE_FORMAT = {
+  datMonth: 'D MMM',
+  yearMonthDay: 'YYYY-MM-D',
+  time: 'HH:mm',
+  fullDate: 'DD/MM/YY HH:mm',
+};
+
+function getRandomArrayElement(items) {
+  return items[Math.floor(Math.random() * items.length)];
+}
+
+//приводит время к адекватному формату
+
+function humanizeTaskDueDate(dueDate) {
+  return dueDate ? dayjs(dueDate).format(DATE_FORMAT) : '';
+}
+
+//продолжительнось поездки
+
+function calculateDuration(startDate, endDate) {
+  return dayjs(endDate).diff(startDate, 'mm');
+}
+
+export { getRandomArrayElement, humanizeTaskDueDate, calculateDuration, DATE_FORMAT };
