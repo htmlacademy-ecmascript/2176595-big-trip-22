65d2aad0ac15e7611d594c5357f4d7c320f581f8
Darina Smorkalova <darina.smorkalova@gmail.com>
2023-12-14 13:36:26 +0300

    Добавляет файл add

--- src/presenter/presenter.js
+++ src/presenter/presenter.js
@@ -3,6 +3,7 @@ import EventItems from '../view/event-list-view.js';
 import SortForm from '../view/sort-view.js';
 import PointView from '../view/point-view.js';
 import EditPointView from '../view/edit-point-view.js';
+import AddPoint from '../view/add-new-point.js';
 
 
 export default class TripPresenter {
@@ -16,6 +17,7 @@ export default class TripPresenter {
     render(this.pointsLIstComponent, this.pointsContainer);
     render(new SortForm(), this.pointsLIstComponent.getElement());
     render(new EditPointView(), this.pointsLIstComponent.getElement());
+    render(new AddPoint(), this.pointsLIstComponent.getElement());
 
     for (let i = 0; i < 3; i++) {
       render(new PointView(), this.pointsLIstComponent.getElement());
